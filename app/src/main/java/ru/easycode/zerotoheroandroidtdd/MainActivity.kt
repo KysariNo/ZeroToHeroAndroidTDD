e52674418b7b9617a4150a5d44f61686a2e594cd
Kondratiev Kirillius <ko.ki.ig1991@gmail.com>
2023-11-11 21:37:41 +0300

    001 completed the task changed the text to text view to "I am an Android Developer!"
      002
    completed the task and assigned the ID text to the view with the modified text
    
      003
    completed the task, changed the container (parent) to LinearLayout
    
      004
    completed the task and added a button to the same liner layout
    
    005
    completed the task, placed the button under the text field by changing the orientation of the container
    
    006
    completed the task, wrote the code in the CT file so that when you press button changeButton , the text changes to text view titleTextView to text "I am an Android Developer!"
    
    007
    completed the task of saving text in text view despite the creation of a new activity

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,44 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
+    private  lateinit var  textView:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.changeButton)
+        textView = findViewById<TextView>(R.id.titleTextView)
+
+        button.setOnClickListener {
+
+            textView.setText(("I am an Android Developer!"))
+        }
     }
-}
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("text" ,textView.text.toString())
+    }
+
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        textView.text = savedInstanceState.getString("text")
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+
+
+    }
+
