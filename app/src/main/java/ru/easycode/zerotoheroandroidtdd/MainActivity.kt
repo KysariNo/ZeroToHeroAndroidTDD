7f6335672232c8e1d400c122b30669bd1555be29
Kondratiev Kirillius <ko.ki.ig1991@gmail.com>
2023-11-30 17:38:31 +0300

    014
    completed the task by clicking on the button, the progressbar animation begins, the text button appears and becomes unclickable

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,29 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.titleTextView)
+        val processBar = findViewById<ProgressBar>(R.id.progressBar)
+        val button = findViewById<Button>(R.id.actionButton)
+
+        button.setOnClickListener {
+            button.isEnabled= false
+            processBar.visibility = View.VISIBLE
+            button.postDelayed({
+                textView.visibility = View.VISIBLE
+                processBar.visibility = View.GONE
+                button.isEnabled=true},3000)}
+
+
+
     }
 }
